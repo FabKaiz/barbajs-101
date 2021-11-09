2e52f235253df363b679f83dbd2b0420e28b9f46
Fabien <cacrfabfr@gmail.com>
2021-11-09 10:19:23 +0100

    fading transition

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -1,1 +1,41 @@
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
 
+// tell Barba to use the css plugin
+barba.use(barbaCss);
+
+const body = document.querySelector('body');
+
+barba.hooks.before((data) => {
+
+  const background = data.current.container.dataset.background;
+  body.style.setProperty('--page-background', background);
+
+});
+
+// init Barba
+barba.init({
+  transitions: [
+    {
+      name: 'home',
+      beforeOnce() {
+        console.log('beforeOnce');
+      },
+
+      once() {
+        console.log('Once');
+      },
+
+      afterOnce() {
+        console.log('afterOnce');
+      },
+    }, {
+      name: 'fade',
+      to: {
+        namespace: ['fade'],
+      },
+      leave() {},
+      enter() {},
+    },
+  ],
+});
