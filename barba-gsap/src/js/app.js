5631c7fee38a2b4614e747d2ca412f74af10067f
Fabien <cacrfabfr@gmail.com>
2021-11-09 11:55:38 +0100

    page load animations

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,11 +1,25 @@
 import barba from '@barba/core';
+import gsap from 'gsap';
+
 import { animationEnter, animationLeave } from './animations';
+
+const resetActiveLinks = () => gsap.set('a.is-active span', {
+  xPercent: -100,
+  transformOrigin: 'left',
+});
 
 barba.init({
   transitions: [
     {
       once({ next }) {
-        animationEnter(next.container);
+        resetActiveLinks();
+        gsap.from('header a', {
+          duration: 0.6,
+          yPercent: 100,
+          stagger: 0.2,
+          ease: 'power1.out',
+          onComplete: () => animationEnter(next.container),
+        });
       },
       leave: ({ current }) => animationLeave(current.container),
       enter({ next }) {
