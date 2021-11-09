d6b15e551c6018f9670f44bac3d1fc8c67dd4592
Fabien <cacrfabfr@gmail.com>
2021-11-09 12:06:25 +0100

    page animation out

--- barba-gsap/src/js/animations/animationLeave.js
+++ barba-gsap/src/js/animations/animationLeave.js
@@ -1,12 +1,32 @@
 import gsap from 'gsap';
 
 const animationLeave = (container) => {
-  return gsap.to(container, {
-    duration: 1,
-    autoAlpha: 0,
-    clearProps: 'all',
-    ease: 'none',
+
+  const activeLink = container.querySelector('a.is-active span');
+  const images = container.querySelectorAll('.image');
+  const img = container.querySelectorAll('img');
+
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 0.4,
+      ease: 'power1.in',
+    },
   });
+
+  tl
+    .to(activeLink, {
+      xPercent: 101,
+    }, 0)
+    .to(images, {
+      xPercent: 101,
+      stagger: 0.05,
+    }, 0)
+    .to(img, {
+      xPercent: -101,
+      stagger: 0.05,
+    }, 0);
+
+  return tl;
 };
 
 export default animationLeave;
