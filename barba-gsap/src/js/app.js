8400867fadf6c6f7975c8484f60db0468681ad75
Fabien <cacrfabfr@gmail.com>
2021-11-09 11:26:35 +0100

    gsap page transition

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,3 +1,35 @@
 import barba from '@barba/core';
 import gsap from 'gsap';
 
+const animationEnter = (container) => {
+  return gsap.from(container, {
+    duration: 1,
+    autoAlpha: 0,
+    clearProps: 'all',
+    ease: 'none',
+  });
+};
+
+const animationLeave = (container) => {
+  return gsap.to(container, {
+    duration: 1,
+    autoAlpha: 0,
+    clearProps: 'all',
+    ease: 'none',
+  });
+};
+
+barba.init({
+  transitions: [
+    {
+      once({ next }) {
+        animationEnter(next.container);
+      },
+      leave: ({ current }) => animationLeave(current.container),
+      enter({ next }) {
+        console.log('entering');
+        animationEnter(next.container);
+      },
+    },
+  ],
+});
